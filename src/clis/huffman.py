6908d4f1f45418fe2d3c7badad04faebc39fe633
Juan Diego Lugo-Ruiz <juan.di77@gmail.com>
2026-05-03 08:50:30 -0700

    feat: initial commit
    Initial round of work, implementing a Huffman compression
    CLI based on the content of C949's zyBook on unit 1.

---
+++ src/clis/huffman.py
@@ -0,0 +1,80 @@
+import json
+from pathlib import Path
+
+import click
+import rich
+from rich import tree, text
+
+from lib.huffman_tree import (
+    HuffmanNode,
+    build_frequency_table,
+    build_huffman_tree,
+    compress_string,
+    decompress_string,
+    get_huffman_codes,
+)
+
+
+@click.group
+def main():
+    pass
+
+@main.command('hello')
+def hello():
+    print("Hello from python-algorithms!")
+
+
+@main.command('count')
+@click.argument('string', type=click.STRING)
+def count(string: str):
+    from pprint import pprint
+    pprint(build_frequency_table(string))
+
+
+@main.command('huffman-tree')
+@click.argument('string', type=click.STRING)
+def huffman_tree(string: str):
+    root = build_huffman_tree(string)
+    codes = dict()
+    get_huffman_codes(root, "", codes)
+    rich_tree = tree.Tree(text.Text(f'({root.frequency})'))
+
+    def _add_children_to_tree(n: HuffmanNode, tree: tree.Tree, include_self: bool = True):
+        # only traverse children if we're an intermediate node
+        if n.is_intermediate():
+            if include_self:
+                tree = tree.add(text.Text(f'({n.frequency})'))
+            for child in (n.left, n.right):
+                if child is None:
+                    continue
+                else:
+                    _add_children_to_tree(child, tree, include_self=True)
+        else:
+            tree.add(text.Text(f'[{n.character}][{n.frequency}][{codes[n.character]}]', style='bold bright_blue'))
+
+    # traverse the tree
+    _add_children_to_tree(root, rich_tree, include_self=False)
+
+    # and print the result
+    rich.print(rich_tree)
+
+
+@main.command('huffman-compress')
+@click.argument('string', type=click.STRING)
+@click.argument('output', type=click.Path(path_type=Path))
+def huffman_compress(string: str, output: Path):
+    new_string, codes = compress_string(string)
+    output.write_text(json.dumps(codes, indent=4))
+    print(f'compressed into: {new_string}')
+    print(f'codes output to: {output}')
+
+
+@main.command('huffman-decompress')
+@click.argument('string', type=click.STRING)
+@click.argument('codes', type=click.Path(path_type=Path))
+def huffman_decompress(string: str, codes):
+    print(f'original string is: {decompress_string(string, json.loads(codes.read_text()))}')
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/lib/huffman_tree.py
@@ -0,0 +1,142 @@
+from dataclasses import dataclass, field
+from queue import PriorityQueue
+import queue
+from typing import Optional
+
+NULL_CHAR = '\0'
+
+
+@dataclass
+class HuffmanNode:
+    left: Optional['HuffmanNode'] = field(default=None)
+    right: Optional['HuffmanNode'] = field(default=None)
+    character: str = field(default=NULL_CHAR)
+    frequency: int = field(default=0)
+
+    def __lt__(self, other: 'HuffmanNode'):
+        return self.frequency < other.frequency
+
+    def is_leaf(self) -> bool:
+        return all(n is None for n in (self.left, self.right))
+
+    def is_intermediate(self) -> bool:
+        return not self.is_leaf()
+
+    @classmethod
+    def create_internal(
+        cls,
+        left: Optional['HuffmanNode'],
+        right: Optional['HuffmanNode'],
+    ) -> 'HuffmanNode':
+        internal_node = cls(left, right)
+        for side in (left, right):
+            if side:
+                internal_node.frequency += side.frequency
+        return internal_node
+
+    @classmethod
+    def create_leaf(cls, character: str, frequency: int) -> 'HuffmanNode':
+        if not character:
+            raise ValueError()
+
+        if frequency == 0:
+            raise ValueError()
+
+        return cls(character=character, frequency=frequency)
+
+
+def build_frequency_table(input_string: str) -> dict[str, int]:
+    map_ct = dict()
+    for letter in input_string:
+        value = map_ct.setdefault(letter, 0)
+        map_ct[letter] = value + 1
+
+    # This isn't really needed since we're relying on ``HuffmanNode``
+    # having a less than impl
+    #sorted_items = sorted(map_ct.items(), key=lambda v: v[1], reverse=True)
+    #return dict(sorted_items)
+    return map_ct
+
+
+def get_huffman_codes(node, prefix, output):
+    if left := node.left:
+        get_huffman_codes(left, prefix + "0", output)
+        if right := node.right:
+            get_huffman_codes(right, prefix + "1", output)
+    else:
+        output[node.character] = prefix
+
+
+def build_huffman_tree(input_string: str) -> 'HuffmanNode':
+    table = build_frequency_table(input_string)
+    nodes = PriorityQueue()
+
+    for character in table:
+        leaf = HuffmanNode.create_leaf(
+            character=character, frequency=table[character]
+        )
+        nodes.put(leaf)
+
+    # Build intermediate nodes.
+    while nodes.qsize() > 1:
+        left = nodes.get()
+        right = nodes.get()
+        intermediate_node = HuffmanNode.create_internal(left, right)
+        nodes.put(intermediate_node)
+
+    return nodes.get()
+
+
+def compress_string(input_string: str) -> tuple[str, dict[str, str]]:
+    """
+    Compress ``input_string`` into a binary string (a sequence of ones and zeroes,)
+    and return that new string alongside a mapping of binary 'characters' to their
+    original value.
+
+    Each characters' binary string is formed by traversing a binary tree from the root
+    node. A left move yields a 0, a right move a 1.
+
+    For example:
+        > compress_string('Sponge Bob')
+        > string => 01011111100010000111001110110101
+        > map    =>
+            {
+              "000": "g",
+              "001": "n",
+              "010": "S",
+              "011": "e",
+              "100": " ",
+              "101": "b",
+              "110": "o",
+              "1110": "B",
+              "1111": "p"
+            }
+    """
+    if not input_string:
+        return input_string, dict()
+
+    root = build_huffman_tree(input_string)
+    codes = dict()
+    get_huffman_codes(root, '', codes)
+
+    # On the way out, revert the map, so we  can scan chars left to right,
+    # and map them to letters.
+    return ''.join(map(lambda c: codes[c], input_string)), {v:k for k,v in codes.items()}
+
+
+def decompress_string(input_string: str, codes: dict[str, str]) -> str:
+    input_queue = queue.Queue()
+
+    for c in input_string:
+        input_queue.put(c)
+
+    k = ''
+    decompressed_string = ''
+
+    while input_queue.qsize() > 0:
+        k += input_queue.get()
+        if k in codes:
+            decompressed_string += codes[k]
+            k = ''
+
+    return decompressed_string
